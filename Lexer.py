66ef92461172a82e754abdbfaaaf407eed0113a0
JuanSC <juansancristobal10@gmail.com>
2024-05-28 11:34:28 -0300

    asdf

--- Lexer.py
+++ Lexer.py
@@ -2,49 +2,54 @@ import ply.lex as lex
 import re
 """ with open('archivo_ejemplo.json') as archivo_empresas:
     datos = archivo_empresas.read() """
+    
+reservadas = ["VERSION","FIRMA_DIGITAL", "EMPRESAS", #"List_empresas" 
+          "NOMBRE_EMPRE","FUNDACION","INGRESOS_ANUALES","PYME",
+          "DIRECCION","CALLE","CIUDAD","PAIS","DEPARTAMENTOS","SUB_DPTOS",#"Lista_Dpto",#"Lista_Sub", "Sub_Dpto",
+          "JEFE","EMPLEADOS", #"Lista_Empleados",
+          "NOMBRE","EDAD","CARGO","SALARIO","ACTIVO","FECHA_CONTRATACION","PROYECTOS",#"Lista_Proyectos",
+          "ESTADO","FEC_INICIO","FEC_FIN"]
 
-tokens = ["Version","FirmaDigital", "Empresas", #"List_empresas" 
-          "Nombre_Empre","Fundacion","Ingresos_Anuales","Pyme",
-          "Direccion","Calle","Ciudad","Pais","Departamentos","Sub_Dptos",#"Lista_Dpto",#"Lista_Sub", "Sub_Dpto",
-          "Jefe","Empleados", #"Lista_Empleados",
-          "Nombre","Edad","Cargo","Salario","Activo","Fec_Contra","Proyectos",#"Lista_Proyectos",
-          "Estado","Fec_Inicio","Fec_Fin","Texto","ApertLista","CierreLista","ApertBloque","CierreBloque","Numero","Booleano",
-          "Fecha","FechaPrueba", "Asignacion"]
+simbolos = ["TEXTO", "ASIGNACION", "COMA","APERT_LISTA","CIERRE_LISTA","APERT_BLOQUE","CIERRE_BLOQUE","NUMERO","BOOLEANO",
+          "FECHA","FECHA_PRUEBA"]
 
-t_Version = r'"version"'
-t_FirmaDigital = r'"firma_digital"'
-t_Empresas = r'"empresas"'
-t_Nombre_Empre = r'"nombre_empresa"'
-t_Fundacion = r'"fundacion"'
-t_Ingresos_Anuales = r'"ingresos_anuales"'
-t_Pyme = r'"pyme":'
-t_Direccion = r'"direccion"'
-t_Calle = r'"calle"'
-t_Ciudad = r'"ciudad"'
-t_Pais = r'"pais"'
-t_Departamentos = r'"departamentos"'
-t_Sub_Dptos = r'"subdepartamentos"'
-t_Jefe = r'"jefe"'
-t_Empleados = r'"empleados"'
-t_Nombre = r'"nombre"'
-t_Edad = r'"edad"'
-t_Cargo = r'"cargo"'
-t_Salario = r'"salario"'
-t_Activo = r'"activo"'
-t_Fec_Contra = r'"fecha_contratacion"'
-t_Proyectos = r'"proyectos"'
-t_Estado = r'"estado"'
-t_Fec_Inicio = r'"fecha_inicio"'
-t_Fec_Fin = r'"fecha_fin"'
-#t_Asignacion = r":"
-t_ApertLista = r'[[]'
-t_CierreLista = r'[]]'
-t_ApertBloque = r'[{]'
-t_CierreBloque = r'[}][,]*'
-t_Texto = r'[:][ ]*["][\w\s]+["]'
-t_Numero = r'[\d]+'
-t_Booleano = r'(true|false)'
-t_Fecha = r'"(19[0-9][0-9]|20[0-9][0-9])-[1,0][0-9]-[0-3][0-9]"'
+tokens = simbolos + reservadas
+
+t_VERSION = r'"version"'
+t_FIRMA_DIGITAL = r'"firma_digital"'
+t_EMPRESAS = r'"empresas"'
+t_NOMBRE_EMPRE = r'"nombre_empresa"'
+t_FUNDACION = r'"fundacion"'
+t_INGRESOS_ANUALES = r'"ingresos_anuales"'
+t_PYME = r'"pyme":'
+t_DIRECCION = r'"direccion"'
+t_CALLE = r'"calle"'
+t_CIUDAD = r'"ciudad"'
+t_PAIS = r'"pais"'
+t_DEPARTAMENTOS = r'"departamentos"'
+t_SUB_DPTOS = r'"subdepartamentos"'
+t_JEFE = r'"jefe"'
+t_EMPLEADOS = r'"empleados"'
+t_NOMBRE = r'"nombre"'
+t_EDAD = r'"edad"'
+t_CARGO = r'"cargo"'
+t_SALARIO = r'"salario"'
+t_ACTIVO = r'"activo"'
+t_FECHA_CONTRATACION = r'"fecha_contratacion"'
+t_PROYECTOS = r'"proyectos"'
+t_ESTADO = r'"estado"'
+t_FEC_INICIO = r'"fecha_inicio"'
+t_FEC_FIN = r'"fecha_fin"'
+t_ASIGNACION = r':'
+t_COMA = r','
+t_APERT_LISTA = r'[[]'
+t_CIERRE_LISTA = r'[]]'
+t_APERT_BLOQUE = r'[{]'
+t_CIERRE_BLOQUE = r'[}]'
+t_NUMERO = r'[\d]+'
+t_BOOLEANO = r'(true|false)'
+t_FECHA = r'["](19[0-9][0-9]|20[0-9][0-9])-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])["]'
+#t_fecha_Invalida = r'[:][ ]*["](\d{4})-(\d{2})-(\d{2})["]'
 
 
 
@@ -62,16 +67,24 @@ def t_nueva_linea(t):
 def t_error(t):
     print("Error de caracter '%s'" % t.value[0])
     t.lexer.skip(1)
+    
+def t_TEXTO(t): #"fecha_contratacion"
+    r'["][\w\s]+["]'
+    if t.value.upper().strip('"') in reservadas:
+        t.type = t.value.upper().strip('"')
+    return t
+
+         
 
 
 lexer = lex.lex()
 
 
-data = '"empresas": [ {\n"nombre_empresa": "MC Donals", "fundacion": 2025, "direccion": { "calle": "Calle Falsa 123", "ciudad": "Springfield", "pais": "USA"},}'
+#data = '"empresas": [ {\n"nombre_empresa": "MC Donals", "fundacion": 2025, "direccion": { "calle": "Calle Falsa 123", "ciudad": "Springfield", "pais": "USA"},}'
 
 #data = '"empresas": [{\n"nombre": "string",\n"version": "integer", "fecha_contratacion": "1999-12-31" }]'
 
-#data = '"fecha_contratacion": "2001-12-31"'
+data = '"fecha_contratacion": "2001-12-45"'
 
 
 #data = '"empresas": ['
